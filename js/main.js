bb8ccc1db5bb83888feb8d0333ae462039b46b11
Christian Bourgeois <christian.bourgeois@appdirect.com>
2014-10-01 19:08:20 -0400

    Added build and move sources around.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  injectSidebar();
-});
-
-function injectSidebar() {
-  $("body").append("<div id='pr_sidebar'></div>");
-  var sidebarHtml = $("#pr_sidebar").load(chrome.extension.getURL('html/sidebar.html'));
-}
