2dbd9b7576d0d07d0fa69b609dd979d673a973d3
Remy Giard <remy.giard@appdirect.com>
2014-10-01 19:36:22 -0400

    use the new chrome extension template

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,63 @@
 ;(function() {
-    console.log('CONTENT SCRIPT WORKS!');
+  var $ = require('./libs/jquery-2.1.1.min');
+  var github = require('./adapter.github');
 
-    var $ = require('./libs/jquery-2.1.1.min');
-    console.log('jQuery version:', $().jquery);
+  $(document).ready(function() {
+    compileTemplates().then(function() {
+      injectSidebar();
+      refreshSidebar();
+    });
+  });
 
-    $(document).ready(function() {
-        injectSidebar();
+  var sidebarTemplate = null;
+
+  function compileTemplates() {
+    return injectTemplate("sidebar", "html/sidebar.html").then(function(template) {
+      sidebarTemplate = template;
+    });
+  }
+
+  function injectTemplate(id, filename) {
+    id += "-template";
+    $("body").append('<script id="' + id + '" type="text/x-handlebars-template"></script>');
+
+    var defer = $.Deferred();
+    $("#" + id).load(chrome.extension.getURL(filename), function() {
+      var html = $("#" + id).html();
+      var Handlebars = require('./libs/handlebars-v2.0.0');
+      defer.resolve(Handlebars.compile(html));
     });
 
-    function injectSidebar() {
-        $("body").append("<div id='pr_sidebar'></div>");
-        var sidebarHtml = $("#pr_sidebar").load(chrome.extension.getURL('html/sidebar.html'));
-    };
+    return defer;
+  }
+
+  function injectSidebar() {
+    $("body").append("<div id='pr-extension'></div>");
+    $("#pr-extension").html(sidebarTemplate({}));
+  }
+
+  function refreshSidebar() {
+    var data = { pullRequests: github.findPullRequests() };
+    $("#pr-extension").html(sidebarTemplate(data));
+    bindEvents();
+  }
+
+
+  function bindToggleHandler() {
+    $("#pr-extension .pr-toggle").off('click');
+    $("#pr-extension .pr-toggle").on('click', function() {
+      $('#pr-extension').toggleClass('pr-ext-toggle');
+    });
+  }
+
+  function bindEvents() {
+    $("#pr_sidebar #pull_requests li").click(function() {
+      var id = $(this).attr("data-pr-id");
+      github.openPullRequest(id);
+    }).hover(function() {
+      $(this).addClass();
+    });
+
+    bindToggleHandler();
+  }
 })();
